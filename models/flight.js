844a7a153d1f4262ba2867c193a76e128a8d3b40
Abi Samson <abi.samson97@yahoo.com>
2023-10-25 11:52:29 -0400

    Flight Lab Pt.1

---
+++ models/flight.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema({
+    airline: { 
+        type: String, 
+        required: true, 
+        enum: ['Southwest', 'United', 'Delta', 'American']
+    },
+    airport: { 
+        type: String, 
+        default: 'DCA', 
+        enum: ['DCA', 'SFA', 'LAX', 'DFW']
+    },
+    flightNo: {
+        type: Number,
+        min: 10,
+        max: 9999,
+        required: true
+    },
+    departs: {
+        type: Date,
+        default: () => {
+          const oneYear = new Date();
+          oneYear.setFullYear(oneYear.getFullYear() + 1);
+          return oneYear;
+        },
+        required: true
+      }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Flight', flightSchema);
